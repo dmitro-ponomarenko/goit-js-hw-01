0ad1809035db4d550c9dc37d76899c2d33a83d0a
dmitro-ponomarenko <svitoyar14@gmail.com>
2020-07-21 21:32:04 +0300

    3 and 4 tasks changes

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,27 @@
 const total = 100;
 let ordered = 50;
-if (ordered<total) {
+if (ordered<=total) {
  console.log("Заказ оформлен, свами свяжется менеджер");   
 }
 else {
  console.log("На складе недостаточно товаром!");   
 }
 ordered = 20;
-if (ordered<total) {
+if (ordered<=total) {
     console.log("Заказ оформлен, свами свяжется менеджер");   
    }
    else {
     console.log("На складе недостаточно товаром!");   
    }
    ordered = 80;
-   if (ordered<total) {
+   if (ordered<=total) {
     console.log("Заказ оформлен, свами свяжется менеджер");   
    }
    else {
     console.log("На складе недостаточно товаром!");   
    }
    ordered = 130;
-   if (ordered<total) {
+   if (ordered<=total) {
     console.log("Заказ оформлен, свами свяжется менеджер");   
    }
    else {
--- js/task-4.js
+++ js/task-4.js
@@ -2,12 +2,17 @@ let credits;
 credits = 23580;
 const pricePerDroid = 3000;
 let quantity = prompt("Введите количество дроидов");
+let totalPrice = quantity * pricePerDroid;//Вирішив розділити загальну ціну і різницю
+let remainder = credits - totalPrice;
 if (quantity === null){
     alert('Отменено пользователем');
 }
-else if (quantity <= 7){
-    alert(`Вы купили ${quantity} дроидов, на счету осталось ${credits%(quantity*pricePerDroid)} кредитов`);
-}
-else {
+else if (totalPrice > credits){
     alert('Недостаточно средств на счету');
 }
+else {
+    alert(`Вы купили ${quantity} дроидов, на счету осталось ${remainder} кредитов`);
+} 
+// Перепрошую, що не зробив так, як Ви мені порадили, в останню мить мені таке
+//рішення даної задачі прийшло, не знаю чи вірно я роблю чи ні, але мені так воно сподобалося,
+//що я просто не можу не поділитися ним з вами
